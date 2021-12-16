108bb46f0a8c6c60d906ad1d255a156691b8621d
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-12-16 22:21:56 +0200

    ✔ FIXED: execl Error not wakking

--- process.c
+++ process.c
@@ -2,8 +2,10 @@
 
 int main(int agrc, char * argv[])
 {
+    printf("\nHELLO FROM PROCCESS.out MYID=%d, MYPPID=%d\n", getpid(), getppid());
     // printf("\nCurrentTime: %ld\n", clock()/CLOCKS_PER_SEC);
     while (clock() < atoi(argv[1])*CLOCKS_PER_SEC);
+    printf("\nI FINISHED\n");
     // printf("\nFinishTime: %ld\n", clock()/CLOCKS_PER_SEC);
     return 0;
 }
--- scheduler.c
+++ scheduler.c
@@ -39,6 +39,8 @@ void sigChildHandler(int signum)
     // Print to LOG file
     printDEBUG(*runningProcessPTR);
     runningProcessPTR = dequeue(&CPQptr);
+    if(runningProcessPTR == NULL)
+        return;
     if(!strcmp(runningProcessPTR->state, "ready"))
     {
         strcpy(runningProcessPTR->state, "started");
@@ -52,7 +54,6 @@ void sigChildHandler(int signum)
     // TODO:
     // Print to LOG file 
     printDEBUG(*runningProcessPTR);
-
 }
 
 int main(int argc, char * argv[])
@@ -83,67 +84,76 @@ int main(int argc, char * argv[])
     // Get Current Work Directory for Process.out
     char processBuffer[500];
     getcwd(processBuffer, sizeof(processBuffer));
-    const char* PROCESS_PATH = strcat(processBuffer, "/process.out");
+    PROCESS_PATH = strcat(processBuffer, "/process.out");
     // Retriveing receivingQueueID
     key_t keyID;
     keyID = ftok("keyfile", QUEUE_KEY);
     int receivingQueueID = msgget(keyID, 0666| IPC_CREAT);
     // The Holy While "Schedular Implementation"
     CPQptr = newCustomPriorityQueue();
-    while(stillSending || runningProcessPTR!=NULL || !isEmpty(&CPQptr))
+    while(stillSending || runningProcessPTR!=NULL)
     {
-        struct msgbuff receivedProccessMSG;
-        int rec_val = msgrcv(receivingQueueID, &receivedProccessMSG, sizeof(struct process), 7, !IPC_NOWAIT);
-        if(rec_val != -1)
+        if(!stillSending)
         {
-            // intialize process struct
-            struct process receivedProcess = receivedProccessMSG.p;
-            strcpy(receivedProcess.state, "ready");
-            receivedProcess.waitingTime = 0;
-            receivedProcess.remainingTime = receivedProcess.runTime;
-            /*
-                > Now we need to know shall we run, or enqueue receivedProcess
-                ready:    NO proccess.out
-                started:  proccess.out Running
-                stopped:  proccess.out EXISTS
-                resumed:  proccess.out Running
-                finished: proccess.out exited
-            */
-            // RECIEVED::CASE 1
-            if(runningProcessPTR==NULL)
+            sleep(1);
+        }
+        else 
+        {
+            struct msgbuff receivedProccessMSG;
+            int rec_val = msgrcv(receivingQueueID, &receivedProccessMSG, sizeof(struct process), 7, !IPC_NOWAIT);
+            if(rec_val != -1)
             {
-                // TODO: run receivedProcess
-                strcpy(runningProcessPTR->state, "started");
-                runningProcessPTR->pID = wakeProccess(runningProcessPTR->runTime);
-                // TODO:
-                // Print to LOG file 
-                printDEBUG(*runningProcessPTR);
-            }
-            // RECIEVED::CASE 2
-            if(runningProcessPTR!=NULL)
-            {
-                // SRTN Case
-                if(insertionFactor==SRTN && runningProcessPTR->remainingTime>receivedProcess.remainingTime)
+                // intialize process struct
+                struct process receivedProcess = receivedProccessMSG.p;
+                strcpy(receivedProcess.state, "ready");
+                receivedProcess.waitingTime = 0;
+                receivedProcess.remainingTime = receivedProcess.runTime;
+                /*
+                    > Now we need to know shall we run, or enqueue receivedProcess
+                    ready:    NO proccess.out
+                    started:  proccess.out Running
+                    stopped:  proccess.out EXISTS
+                    resumed:  proccess.out Running
+                    finished: proccess.out exited
+                */
+                // RECIEVED::CASE 1
+                if(runningProcessPTR==NULL)
                 {
-                    // TODO: replace runningProcess with receivedProcess
-                    // [1]: stop runningProcess
-                    strcpy(runningProcessPTR->state, "stopped");
-                    kill(runningProcessPTR->pID, SIGSTOP);
-                    enqueue(&CPQptr, runningProcessPTR, insertionFactor);
-                    // [2]: Print to LogFile
-                    printDEBUG(*runningProcessPTR);
-                    // [3]: receivedProcess->state "started"
+                    // TODO: run receivedProcess
                     runningProcessPTR = &receivedProcess;
+                    printDEBUG(*runningProcessPTR);
                     strcpy(runningProcessPTR->state, "started");
-                    // [4]: wakeProccess(int runTime, const char* PROCESS_PATH)
                     runningProcessPTR->pID = wakeProccess(runningProcessPTR->runTime);
-                    // [5]: Print to LogFile
+                    // TODO:
+                    // Print to LOG file 
                     printDEBUG(*runningProcessPTR);
                 }
-                else
+                // RECIEVED::CASE 2
+                else if(runningProcessPTR!=NULL)
                 {
-                    // Enqueue HPF, SRTN, RR, SJF FCFS Case
-                    enqueue(&CPQptr, &receivedProcess, insertionFactor);
+                    // SRTN Case
+                    if(insertionFactor==SRTN && runningProcessPTR->remainingTime>receivedProcess.remainingTime)
+                    {
+                        // TODO: replace runningProcess with receivedProcess
+                        // [1]: stop runningProcess
+                        strcpy(runningProcessPTR->state, "stopped");
+                        kill(runningProcessPTR->pID, SIGSTOP);
+                        enqueue(&CPQptr, runningProcessPTR, insertionFactor);
+                        // [2]: Print to LogFile
+                        printDEBUG(*runningProcessPTR);
+                        // [3]: receivedProcess->state "started"
+                        runningProcessPTR = &receivedProcess;
+                        strcpy(runningProcessPTR->state, "started");
+                        // [4]: wakeProccess(int runTime, const char* PROCESS_PATH)
+                        runningProcessPTR->pID = wakeProccess(runningProcessPTR->runTime);
+                        // [5]: Print to LogFile
+                        printDEBUG(*runningProcessPTR);
+                    }
+                    else
+                    {
+                        // Enqueue HPF, SRTN, RR, SJF FCFS Case
+                        enqueue(&CPQptr, &receivedProcess, insertionFactor);
+                    }
                 }
             }
         }
